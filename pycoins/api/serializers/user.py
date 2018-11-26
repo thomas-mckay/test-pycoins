88357fcbdec27a105b05647a5a174c95f23c19b4
Thomas Mc Kay <thomas.d.mckay@gmail.com>
2018-11-26 02:31:03 +0100

    feat (api): add password to user writable fields

--- pycoins/api/serializers/user.py
+++ pycoins/api/serializers/user.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.HyperlinkedIdentityField(view_name='api:user-detail')
+    password = serializers.CharField(required=False, write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email')
+
+    def validate_password(self, value):
+        return make_password(value)
